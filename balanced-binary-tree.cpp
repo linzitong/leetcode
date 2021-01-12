f12c6d2b121afd2b7056b0424b346dbdb7ab4e20
linzitong <1361377791@qq.com>
2021-01-12 19:43:08 +0800

    Update balanced-binary-tree.cpp

--- balanced-binary-tree.cpp
+++ balanced-binary-tree.cpp
@@ -1,3 +1,4 @@
+//题目链接https://leetcode-cn.com/problems/balanced-binary-tree/solution/ping-heng-er-cha-shu-by-leetcode-solution/
 //给定一个二叉树，判断它是否是高度平衡的二叉树。
 //本题中，一棵高度平衡二叉树定义为：
 //一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
